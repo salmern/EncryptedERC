9825430f6c265ee29d3172573338168b6579a720
Mehmet Berat Öztürk <mehmet.ozturk@avalabs.org>
2025-03-24 12:08:34 +0300

    feat: adds biome as ts linter (#44)
    * feat: adds biome for ts linter
    
    * chore: fixes lint issues
    
    * feat: adds new script for biome lint
    
    * chore: updates gh-action for sol & ts lint
    
    * chore: adds hardhat/typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,14 @@
 import type { HardhatUserConfig } from "hardhat/config";
+import "@typechain/hardhat";
 import "solidity-coverage";
 import "hardhat-gas-reporter";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomicfoundation/hardhat-ethers";
 
+import dotenv from "dotenv";
+dotenv.config();
 
-import dotenv from "dotenv"
-dotenv.config()
-
-const RPC_URL = process.env.RPC_URL || "https://api.avax.network/ext/bc/C/rpc"
+const RPC_URL = process.env.RPC_URL || "https://api.avax.network/ext/bc/C/rpc";
 
 const config: HardhatUserConfig = {
 	solidity: {
@@ -26,17 +26,17 @@ const config: HardhatUserConfig = {
 			forking: {
 				url: RPC_URL,
 				blockNumber: 59121339,
-				enabled: process.env.FORKING ? true : false,
+				enabled: !!process.env.FORKING,
 			},
 		},
 	},
 	gasReporter: {
-		enabled: process.env.REPORT_GAS ? true : false,
+		enabled: !!process.env.REPORT_GAS,
 		currency: "USD",
 		coinmarketcap: process.env.COINMARKETCAP_API_KEY,
 		excludeContracts: ["contracts/mocks/"],
 		outputFile: "gas-report.txt",
-		L1: 'avalanche',
+		L1: "avalanche",
 		showMethodSig: true,
 	},
 };
